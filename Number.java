e78ae3b2fc2640ebd4aee11d7341a24d5c8b5e64
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-27 18:04:25 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,16 @@
+package janani;
+import java.util.Scanner;
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n,n1,n2,n3;
+Scanner ja=new Scanner(System.in);
+n1=ja.nextInt();
+n2=ja.nextInt();
+n3=ja.nextInt();
+n=(n3/2)*((2*n1)+(n3-1)*n2);
+System.out.println(n);
+	}
+
+}
